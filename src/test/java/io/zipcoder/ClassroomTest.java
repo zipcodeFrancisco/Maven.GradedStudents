f88a5368cc4fa3c0ac489efa40b23ecadd0d2402
Leon <leon@Leons-MacBook-Pro.local>
2018-02-15 16:14:25 -0500

    updated src files

--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,7 +1,4 @@
 package io.zipcoder;
 
-import org.junit.Test;
-import org.junit.Assert;
-
 public class ClassroomTest {
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,8 +1,5 @@
 package io.zipcoder;
 
-import org.junit.Test;
-import org.junit.Assert;
-
 public class StudentTest {
 
 }
